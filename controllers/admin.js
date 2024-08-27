ac1641ab9796f5b5e471d2a753305bdd30aa9fb4
SharmarAndrey <sharnarandrey@gmail.com>
2024-08-27 21:53:19 +0200

    Funcionalidad para anadir apartamentos a base de datos

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,31 +1,39 @@
 const Apartment = require('../models/apartment.model.js');
 
-// Функция для рендеринга формы добавления нового апартамента
+// Function  para render de form de nuevo apartamento 
 const getNewApartmentForm = (req, res) => {
 	res.render('new-apartment.ejs');
 }
 
-// Функция для создания нового апартамента
+//function para anadir New Apartment
 const createNewApartment = async (req, res) => {
 	try {
+		// Convert checkbox values to boolean
+		const services = {
+			wifi: req.body.wifi === 'on',
+			airConditioner: req.body.airConditioner === 'on',
+			kitchen: req.body.kitchen === 'on',
+			disability: req.body.disability === 'on',
+			heater: req.body.heater === 'on',
+			tv: req.body.tv === 'on'
+		};
+
+		// Create a new Apartment object with the converted values
 		const newApartment = new Apartment({
 			title: req.body.title,
 			price: req.body.price,
 			size: req.body.size,
 			mainPhoto: req.body.mainPhoto,
-			services: {
-				wifi: req.body.wifi || false,
-				airConditioner: req.body.airConditioner || false,
-				kitchen: req.body.kitchen || false,
-				disability: req.body.disability || false,
-				heater: req.body.heater || false,
-				tv: req.body.tv || false
-			}
+			services: services
 		});
 
+		// Save the new apartment to the database
 		await newApartment.save();
+
+		// Respond to the client with a success message
 		res.status(201).json({ message: 'Apartment created successfully!' });
 	} catch (error) {
+		// Handle errors and respond to the client
 		res.status(500).json({ message: 'Failed to create apartment', error: error.message });
 	}
 }
