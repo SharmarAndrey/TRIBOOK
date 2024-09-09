5b71029561a67fee379903d7b374ef3b66d7f36e
SharmarAndrey <sharnarandrey@gmail.com>
2024-09-09 22:22:39 +0200

    Dark mode

--- public/main.js
+++ public/main.js
@@ -1,15 +1,18 @@
-const toggleSwitch = document.getElementById('darkModeToggle');
-const body = document.body;
+document.addEventListener('DOMContentLoaded', () => {
+	const toggleSwitch = document.getElementById('darkModeToggle');
+	const body = document.body;
 
-if (localStorage.getItem('darkMode') === 'enabled') {
-	body.classList.add('dark-mode');
-}
-
-toggleSwitch.addEventListener('click', () => {
-	body.classList.toggle('dark-mode');
-	if (body.classList.contains('dark-mode')) {
-		localStorage.setItem('darkMode', 'enabled');
-	} else {
-		localStorage.removeItem('darkMode');
+	// Initialize dark mode based on previous setting
+	if (localStorage.getItem('darkMode') === 'enabled') {
+		body.classList.add('dark-mode');
 	}
+
+	toggleSwitch.addEventListener('click', () => {
+		body.classList.toggle('dark-mode');
+		if (body.classList.contains('dark-mode')) {
+			localStorage.setItem('darkMode', 'enabled');
+		} else {
+			localStorage.removeItem('darkMode');
+		}
+	});
 });
